dcd88209688f098efb2dea5c6a2a5607d208cc20
goutham Kishore <send2kgk3@gmail.com>
2021-08-07 13:21:25 +0530

    changes

--- myapp/App.view.js
+++ myapp/App.view.js
@@ -1,14 +1,21 @@
-import React from "react";
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
 import { Provider } from "react-redux";
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 export default function (props, store, state, styles, className, storeState) {
+    const javascript = `window.android={enabled:true};`;
     return (
         <Provider store={storeState}>
-            <View style={styles.container}>
+            {/* <View style={styles.container}>
                 <Text>My First React Native APP</Text>
                 <StatusBar style="auto" />
-            </View>
+            </View> */}
+            <WebView
+                style={styles.container}
+                source={{ uri: 'https://findmycabs.com' }}
+                onError={(e) => alert('Error', e.nativeEvent.description)}
+                injectedJavaScriptBeforeContentLoaded={javascript}
+            />
         </Provider>
     )
 }
